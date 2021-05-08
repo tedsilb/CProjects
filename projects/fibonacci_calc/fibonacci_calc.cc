76287748d9daa754fdf61c3e5d29ebb4b0f15d41
Ted Silbernagel <26172399+tedsilb@users.noreply.github.com>
2021-05-08 13:47:00 -0500

    Small change

--- projects/fibonacci_calc/fibonacci_calc.cc
+++ projects/fibonacci_calc/fibonacci_calc.cc
@@ -6,22 +6,21 @@ long double FibonacciGen(int n_term) {
   if (n_term < 0) {
     return -1;
   }
-
   if (n_term == 0) {
     return 0;
-  } else {
-    long double current_minus_two = 0;
-    long double current_minus_one = 0;
-    long double current = 1;
-
-    for (int i = 1; i < n_term; i++) {
-      current_minus_two = current_minus_one;
-      current_minus_one = current;
-      current = current_minus_two + current_minus_one;
-    }
-
-    return current;
   }
+
+  long double current_minus_two = 0;
+  long double current_minus_one = 0;
+  long double current = 1;
+
+  for (int i = 1; i < n_term; i++) {
+    current_minus_two = current_minus_one;
+    current_minus_one = current;
+    current = current_minus_two + current_minus_one;
+  }
+
+  return current;
 }
 
 int main(int argc, char* argv[]) {
